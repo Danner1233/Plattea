b3e2f4e630493665514803ec9cdfcb3ae181b58d
Danner1233 <dannerariasmunive11@gmail.com>
2024-07-24 18:56:05 -0500

    Creacion de entorno

---
+++ src/main/java/co/edu/sena/plattea/PlatteaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena.plattea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatteaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatteaApplication.class, args);
+    }
+
+}
